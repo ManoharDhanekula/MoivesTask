352473cd8c005cf58d6e02c1fa7a4114ed986c97
ManoharDhanekula <manohard0716@gmail.com>
2024-02-14 23:43:44 +0530

    search and sort

--- controller/mobile.controller.js
+++ controller/mobile.controller.js
@@ -1,23 +1,40 @@
+import { Mobile } from "../model/mobile.model.js";
 import mobileService from "../service/mobile.service.js";
+import usersService from "../service/users.service.js";
 
 async function postDataByID(request, response) {
   console.log(request.body);
-
-  const { brand, title, url, rating, image, os, ram, price } = request.body;
-  const mobileDataPost = mobileService.postingData(
-    brand,
-    title,
-    url,
-    rating,
-    image,
-    os,
-    ram,
-    price
+  const tokenId = request.header("x-auth-token");
+  const sessionToken = await usersService.sessionCheckToken(tokenId);
+  const findingUserRoleID = await usersService.checkingRoleID(
+    sessionToken.dataValues.user_id
   );
-  const not_Found = { msg: "Not Inserted" };
-  mobileDataPost
-    ? response.send(await mobileDataPost)
-    : response.status(404).send(not_Found);
+  console.log(sessionToken.dataValues.user_id);
+  // const findingRoleData = await usersService.checkingRoleDatabyId(findingUserRoleID.dataValues.role_id)
+  console.log(findingUserRoleID.dataValues.role_id);
+  if (findingUserRoleID.dataValues.role_id == 3) {
+    try {
+      const { brand, title, url, rating, image, os, ram, price } = request.body;
+      const mobileDataPost = mobileService.postingData(
+        brand,
+        title,
+        url,
+        rating,
+        image,
+        os,
+        ram,
+        price
+      );
+      const not_Found = { msg: "Not Inserted" };
+      mobileDataPost
+        ? response.send(await mobileDataPost)
+        : response.status(404).send(not_Found);
+    } catch (err) {
+      response.send({ msg: err });
+    }
+  } else {
+    response.send("NoT Authorization");
+  }
 }
 
 async function paginationForMobilesData(request, response) {
@@ -43,6 +60,55 @@ async function paginationForMobilesData(request, response) {
   }
 }
 
+async function sortByItems(req, res) {
+  try {
+    const order = req.query.order || "DESC";
+    const sortedData = await Mobile.findAll({
+      order: [["rating", order.toUpperCase()]],
+    });
+
+    res.send(sortedData);
+  } catch (error) {
+    console.error("Error fetching sorted data:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+async function priceByItems(req, res) {
+  try {
+    const order = req.query.order || "DESC";
+    const sortedData = await Mobile.findAll({
+      order: [["price", order.toUpperCase()]],
+    });
+
+    res.send(sortedData);
+  } catch (error) {
+    console.error("Error fetching sorted data:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+async function ramByItems(req, res) {
+  try {
+    const searchTerm = req.query.q; // Get the search term from the query parameters
+
+    if (!searchTerm) {
+      return res.status(400).json({ error: "Search term is required" });
+    }
+
+    const searchData = await Mobile.findAll({
+      where: {
+        ram: searchTerm,
+      },
+    });
+
+    res.send(searchData);
+  } catch (error) {
+    console.error("Error searching data:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+}
+
 async function getMobileDataByID(request, response) {
   const not_Found = { msg: "Not Found" };
   const { id } = request.params;
@@ -62,38 +128,72 @@ async function getMobileDataByID(request, response) {
 async function putMobileDataByID(request, response) {
   console.log(request.body);
   const { id } = request.params;
-
-  const { brand, title, url, rating, image, os, ram, price } = request.body;
-  const mobileDataUpdate = mobileService.updatingMobileDataByID(
-    brand,
-    title,
-    url,
-    rating,
-    image,
-    os,
-    ram,
-    price,
-    id
+  const tokenId = request.header("x-auth-token");
+  const sessionToken = await usersService.sessionCheckToken(tokenId);
+  const findingUserRoleID = await usersService.checkingRoleID(
+    sessionToken.dataValues.user_id
   );
-  const not_Found = { msg: "Not Updated" };
-  mobileDataUpdate
-    ? response.send(await mobileDataUpdate)
-    : response.status(404).send(not_Found);
+  console.log(sessionToken.dataValues.user_id);
+  // const findingRoleData = await usersService.checkingRoleDatabyId(findingUserRoleID.dataValues.role_id)
+  console.log(findingUserRoleID.dataValues.role_id);
+  if (findingUserRoleID.dataValues.role_id == 3) {
+    try {
+      const { brand, title, url, rating, image, os, ram, price } = request.body;
+      const mobileDataUpdate = mobileService.updatingMobileDataByID(
+        brand,
+        title,
+        url,
+        rating,
+        image,
+        os,
+        ram,
+        price,
+        id
+      );
+      const not_Found = { msg: "Not Updated" };
+      mobileDataUpdate
+        ? response.send(await mobileDataUpdate)
+        : response.status(404).send(not_Found);
+    } catch (err) {
+      response.send({ msg: err });
+    }
+  } else {
+    response.send("NoT Authorization");
+  }
 }
 
 async function deleteMobileDataByID(request, response) {
   const { id } = request.params;
-  const mobileDataDelete = mobileService.distoryMobileDataByID(id);
-  console.log(mobileDataDelete);
-  const not_Found = { msg: "Not found" };
-  mobileDataDelete
-    ? response.send("Deleted")
-    : response.status(404).send(not_Found);
+  const tokenId = request.header("x-auth-token");
+  const sessionToken = await usersService.sessionCheckToken(tokenId);
+  const findingUserRoleID = await usersService.checkingRoleID(
+    sessionToken.dataValues.user_id
+  );
+  console.log(sessionToken.dataValues.user_id);
+  // const findingRoleData = await usersService.checkingRoleDatabyId(findingUserRoleID.dataValues.role_id)
+  console.log(findingUserRoleID.dataValues.role_id);
+  if (findingUserRoleID.dataValues.role_id == 3) {
+    try {
+      const mobileDataDelete = mobileService.distoryMobileDataByID(id);
+      console.log(mobileDataDelete);
+      const not_Found = { msg: "Not found" };
+      mobileDataDelete
+        ? response.send("Deleted")
+        : response.status(404).send(not_Found);
+    } catch (err) {
+      response.send({ msg: err });
+    }
+  } else {
+    response.send("NoT Authorization");
+  }
 }
 
 export default {
   postDataByID,
   paginationForMobilesData,
+  sortByItems,
+  priceByItems,
+  ramByItems,
   getMobileDataByID,
   putMobileDataByID,
   deleteMobileDataByID,
--- routes/mobile.routes.js
+++ routes/mobile.routes.js
@@ -9,7 +9,9 @@ route
   // .get(mobileController.getAllMobileData)
   .post(auth, mobileController.postDataByID)
   .get(auth, mobileController.paginationForMobilesData);
-
+route.route("/sortBy").get(auth, mobileController.sortByItems);
+route.route("/price").get(auth, mobileController.priceByItems);
+route.route("/search").get(auth, mobileController.ramByItems);
 route
   .route("/:id")
   .get(auth, mobileController.getMobileDataByID)
