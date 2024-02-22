b45a7f6ecaa5f9e4f0bd714c6ce4c83ab99d2ef0
ManoharDhanekula <manohard0716@gmail.com>
2024-02-22 14:36:21 +0530

    Update on Rating in mobiles

--- service/review.service.js
+++ service/review.service.js
@@ -11,26 +11,34 @@ async function postingData(user_id, mobile_id, title_Of, rating, comments) {
   });
 }
 
-async function averageOfRating(mobile_id) {
-  const sumOF = await review.sum("rating", { where: { mobile_id: mobile_id } });
-  const countOF = await review.count({
+async function sumOF(mobile_id) {
+  return await review.sum("rating", { where: { mobile_id: mobile_id } });
+}
+
+async function countOF(mobile_id) {
+  return await review.count({
     where: { mobile_id: mobile_id },
   });
+}
 
-  return sumOF / countOF;
+async function averageOF(mobile_id) {
+  console.log(await sumOF(mobile_id), await countOF(mobile_id));
+  return (await sumOF(mobile_id)) / (await countOF(mobile_id));
 }
 
 async function updateMobileRatings(mobile_id) {
   try {
+    const average = await averageOF(mobile_id);
+    console.log(average);
     const results = await Mobile.update(
-      { rating: await averageOfRating(mobile_id) },
+      { rating: average },
       {
         where: {
           id: mobile_id,
         },
       }
     );
-    console.log(averageOfRating(mobile_id));
+    // console.log(await averageOF(mobile_id));
     console.log("Updated mobile ratings:", results);
   } catch (error) {
     console.error("Error updating mobile ratings:", error);
